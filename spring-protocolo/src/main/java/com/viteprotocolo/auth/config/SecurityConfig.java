0b022b41905eb86fda1db1d50a14f0e27638e898
GugsFranca <guto602@gmail.com>
2026-05-05 15:42:06 -0300

    Remove unused UserResponse and UserRepository classes; update SecurityConfig to maintain CSRF settings

--- spring-protocolo/src/main/java/com/viteprotocolo/auth/config/SecurityConfig.java
+++ spring-protocolo/src/main/java/com/viteprotocolo/auth/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(AbstractHttpConfigurer::disable) // Garanta que está desabilitado
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/auth/**").permitAll()
